9958e09d97d8a48a8055c6ab4bf9410d3de2c9dc
Gunnar Hillert <gunnar@hillert.com>
2023-03-29 10:49:34 -1000

    Add Coherence Spring War sample application

---
+++ src/main/java/com/coherence/tomcat/TomcatApplication.java
@@ -0,0 +1,56 @@
+package com.coherence.tomcat;
+
+import com.oracle.coherence.spring.configuration.annotation.CoherenceCache;
+import com.tangosol.net.Coherence;
+import com.tangosol.net.NamedCache;
+import com.tangosol.net.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.ServletContext;
+
+@SpringBootApplication
+public class TomcatApplication implements ApplicationRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TomcatApplication.class);
+
+	@CoherenceCache("myCacheName")
+	private NamedCache<Long, String> namedCache;
+
+	@Autowired
+	private Coherence coherence;
+
+	@Autowired
+	private ServletContext context;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		namedCache.put(1L, "Hello World");
+		LOGGER.info("context: '{}'", this.context.getContextPath());
+
+		final String cacheNameToUse;
+
+		if (StringUtils.hasText(context.getContextPath())) {
+			cacheNameToUse = context.getContextPath().substring(1);
+		}
+		else {
+			cacheNameToUse = "root";
+		}
+
+		NamedCache<Integer, String> namedCache = coherence.getSession().getCache(cacheNameToUse);
+
+		for (int i = 1; i<=10000; i++) {
+			namedCache.put(i, "Hello Coherence Spring");
+		}
+	}
+}
